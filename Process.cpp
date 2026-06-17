24e9e9de8b7865ba3805a8ff296565a24941b481
Gabriel Villas <gabriel_villas@dlsu.edu.ph>
2026-06-17 21:59:12 +0800

    add process class

---
+++ Process.cpp
@@ -0,0 +1,20 @@
+#include "Process.h"
+#include <stdio.h>
+#include <string>
+using namespace std;
+Process::Process(string name)
+{
+    process_name = name;
+    state = READY;
+    current_instruction = 0;
+    total_instructions = 100;
+    for (int i =0; i < 100; i++)
+    {
+        print_commands.push_back(
+            "Print Command "
+        );
+    }
+
+
+
+}
---
+++ process.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <stdio.h>
+using namespace std;
+enum State
+{
+    READY,
+    RUNNING,
+    FINISHED
+};
+class Process
+{
+public:
+
+    string process_name;
+    State state;
+    int current_instruction;
+    int total_instructions;
+    vector<string> print_commands;
+    Process(string name);
+
+
+
+
+};
